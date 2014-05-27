16c185848d743b46b20f5ced0f13a0e622fe8262
son <son@lincolnix.net>
2014-05-27 22:41:46 +0000

    minor

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,8 +12,6 @@ Bundler.require(*Rails.groups)
 
 module PrairieHillWebsite
   class Application < Rails::Application
-
-    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.stylesheets false
