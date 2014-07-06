f7dee71fe183373b844cc82547d1fceb21c4f42c
son <son@lincolnix.net>
2014-07-06 20:25:22 +0000

    minor

--- db/migrate/20140706162714_add_volunteer_string_array_attribute.rb
+++ db/migrate/20140706162714_add_volunteer_string_array_attribute.rb
@@ -1,5 +1,5 @@
 class AddVolunteerStringArrayAttribute < ActiveRecord::Migration
   def change
-    add_column :shifts, :volunteers, :text, array: true, default: '[]'
+    #add_column :shifts, :volunteers, :text, array: true, default: '[]'
   end
 end
