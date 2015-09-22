7d544459f11d193a8cb2aa5e3350d595140289c0
son <son@lincolnix.net>
2015-09-22 07:44:45 +0000

    asset cdn setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "d3lyuvqqk13ili.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
