5aac97fcfd594e29656ae174b072a92ee9557844
AnderSon <son@lincolnix.net>
2016-08-24 11:51:55 -0500

    remove serializer references

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,5 @@
-class ActivitySerializer < ActiveModel::Serializer
+# class ActivitySerializer < ActiveModel::Serializer
   
-  attributes :id, :work_area, :coordinator, :sign, :num_tickets, :vol_needed, :shift_ids, :created_at, :updated_at, :comments, :start_time, :end_time, :category
+#   attributes :id, :work_area, :coordinator, :sign, :num_tickets, :vol_needed, :shift_ids, :created_at, :updated_at, :comments, :start_time, :end_time, :category
   
-end
+# end
